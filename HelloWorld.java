aa3aaf5d468b8a64de314907519fd3053a334a13
blyatkid <97328797+blyatkid@users.noreply.github.com>
2022-01-18 17:16:34 +0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! Check commit change");
+    System.out.println("Hello world! Commit Changed again!");
   }
 }
